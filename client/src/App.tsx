95f2c4579d3697930365a70b60e64cdcc84f9611
jshandil <jshandil@usc.edu>
2024-01-05 13:43:10 -0800

    commiting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ const API_URL = "http://localhost:8080";
 
 function App() {
   const [data, setData] = useState<string>();
+  const [message, setMessage] = useState<string>('');
 
   useEffect(() => {
     getData();
@@ -26,35 +27,58 @@ function App() {
     });
 
     await getData();
-  };
+    setData(''); // Clear the input field by setting data to an empty string
+};
 
-  const verifyData = async () => {
-    throw new Error("Not implemented");
+const recoverData = async () => {
+  const response = await fetch(`${API_URL}/recover`);
+  if (response.status === 200) {
+    const { data } = await response.json();
+    setData(data);
+    setMessage("Data has been recovered.");
+  } else {
+    setMessage("Data recovery failed. Data not found.");
+  }
+};
+
+
+const verifyData = async () => {
+    const response = await fetch(`${API_URL}/verify`, {
+      method: "POST",
+      body: JSON.stringify({ data }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.status === 200) {
+      setMessage("Data is intact");
+    } else {
+      setMessage("Data has been tampered with");
+    }
+
+    setData('');
   };
 
   return (
     <div
       style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        position: "absolute",
-        padding: 0,
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        gap: "20px",
-        fontSize: "30px",
+        // ... existing styles ...
       }}
     >
-      <div>Saved Data</div>
-      <input
+<div><b>Enter your new information and click 'Update Data' to send it to the server for storage.</b> </div><br></br>
+<div><i>If you want to make sure your data hasn't been changed in any way, simply input the information you saved previously and click 'Verify Data.' The server will then check if your data remains unaltered or if any changes have been made.      
+</i></div><br></br>
+  <div>Saved Data</div>
+
+ <input
         style={{ fontSize: "30px" }}
         type="text"
         value={data}
         onChange={(e) => setData(e.target.value)}
       />
-
+<br></br>
       <div style={{ display: "flex", gap: "10px" }}>
         <button style={{ fontSize: "20px" }} onClick={updateData}>
           Update Data
@@ -62,8 +86,15 @@ function App() {
         <button style={{ fontSize: "20px" }} onClick={verifyData}>
           Verify Data
         </button>
+        
+        <button style={{ fontSize: "20px" }} onClick={recoverData}>
+  Recover Data
+</button>
+
       </div>
+      {message && <div style={{ marginTop: "20px", fontSize: "20px" }}>{message}</div>}
     </div>
+    
   );
 }
 
--- server/app.ts
+++ server/app.ts
@@ -1,24 +1,128 @@
+// import express from "express";
+// import cors from "cors";
+// import crypto from "crypto"; // Importing crypto module for hashing
+
+// const PORT = 8080;
+// const app = express();
+// const database = { hash: "" }; // Storing hash instead of raw data
+
+// app.use(cors());
+// app.use(express.json());
+
+// // Function to create hash
+// const hashData = (data: string): string => {
+//   return crypto.createHash('sha256').update(data).digest('hex');
+// };
+
+// // Routes
+
+// app.get("/", (req, res) => {
+//   res.json({ hash: database.hash });
+// });
+
+// app.post("/", (req, res) => {
+//   const hashedData = hashData(req.body.data);
+//   database.hash = hashedData;
+//   res.sendStatus(200);
+// });
+
+// // Add a new route for data recovery
+// app.get("/recover", (req, res) => {
+//   if (database.hash !== "") {
+//     // If there is a valid hash in the database, return the original data
+//     res.json({ data: database.hash });
+//   } else {
+//     // If there is no valid data in the database, return an error message
+//     res.status(404).json({ error: "Data not found" });
+//   }
+// });
+
+
+// // Verify data route
+// app.post("/verify", (req, res) => {
+//   const hashedData = hashData(req.body.data);
+//   if (hashedData === database.hash) {
+//     res.sendStatus(200); // Data is intact
+//   } else {
+//     res.sendStatus(409); // Data tampered
+//   }
+// });
+
+// app.listen(PORT, () => {
+//   console.log("Server running on port " + PORT);
+// });
+
 import express from "express";
 import cors from "cors";
+import crypto from "crypto";
+import { createCipheriv, createDecipheriv, randomBytes } from "crypto"; // Import crypto functions
 
 const PORT = 8080;
 const app = express();
-const database = { data: "Hello World" };
+const database = { encryptedData: "", iv: "", key: "" };
 
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// Generate a random encryption key and initialization vector (IV)
+const generateEncryptionKeyAndIV = () => {
+  const key = randomBytes(32); // 256 bits key
+  const iv = randomBytes(16); // 128 bits IV
+  return { key, iv };
+};
+
+// Encrypt data
+const encryptData = (data: string, key: Buffer, iv: Buffer): string => {
+  const cipher = createCipheriv("aes-256-cbc", key, iv);
+  const encrypted = cipher.update(data, "utf8", "hex") + cipher.final("hex");
+  return encrypted;
+};
+
+// Decrypt data
+// Decrypt data
+const decryptData = (encryptedData: string, key: Buffer, iv: Buffer): string => {
+  const decipher = createDecipheriv("aes-256-cbc", key, iv);
+  const decrypted = decipher.update(encryptedData, "hex", "utf8") + decipher.final("utf8");
+  return decrypted;
+};
+
 
 app.get("/", (req, res) => {
-  res.json(database);
+  res.json({ encryptedData: database.encryptedData });
 });
 
 app.post("/", (req, res) => {
-  database.data = req.body.data;
+  const { data } = req.body;
+  const { key, iv } = generateEncryptionKeyAndIV();
+  const encryptedData = encryptData(data, key, iv);
+  database.encryptedData = encryptedData;
+  database.iv = iv.toString("hex");
+  database.key = key.toString("hex");
   res.sendStatus(200);
+});
+
+// Verify data route
+app.post("/verify", (req, res) => {
+  const { data } = req.body;
+  const decryptedData = decryptData(database.encryptedData, Buffer.from(database.key, "hex"), Buffer.from(database.iv, "hex"));
+  if (data === decryptedData) {
+    res.sendStatus(200); // Data is intact
+  } else {
+    res.sendStatus(409); // Data tampered
+  }
+});
+
+// Data recovery route
+app.get("/recover", (req, res) => {
+  if (database.encryptedData !== "") {
+    const decryptedData = decryptData(database.encryptedData, Buffer.from(database.key, "hex"), Buffer.from(database.iv, "hex"));
+    res.json({ data: decryptedData });
+  } else {
+    res.status(404).json({ error: "Data not found" });
+  }
 });
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+
